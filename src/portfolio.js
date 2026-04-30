8ae847e8d3495d41ed7cd73067ef334b8cdb272c
Ricky Villaverde <rickyvillaverde3@gmail.com>
2026-04-29 20:24:54 -0400

    Customize Portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,21 +1,21 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://Moojestico.github.io/school-portfolios',
+  title: 'RV.',
 }
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Ricky Villaverde',
+  role: 'Data Engineer',
   picture: 'https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png',
 
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    'Information Technology graduate with experience in data engineering, data analysis, and software development. Proficient in Python, SQL, and google cloud platforms. Passionate about leveraging data to drive insights and solve complex problems.',
+  resume: 'RVResume2026.pdf',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/ricky-villaverde/',
+    github: 'https://github.com/Moojestico',
   },
 }
 
@@ -23,53 +23,52 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'SFTP to GCS Data Pipeline',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-    image: 'cleanfolio.png',
+      'Designed and implemented an automated pipeline to transfer client data from SFTP to Google Cloud Storage using Airflow (Cloud Composer), improving storage scalability and reducing VM disk usage issues.',
+    stack: ['GCP', 'Airflow', 'Python', 'GCS'],
+    sourceCode: '',
+    livePreview: '',
   },
   {
-    name: 'Project 2',
+    name: 'Client Consumer Data Automation',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-    image: 'https://github.githubassets.com/assets/GitHub-Logo-ee398b662d42.png',
+      'Built a Cloud Run-based pipeline to decrypt, process, and load large encrypted transaction files into BigQuery, replacing a manual workflow and improving reliability and performance.',
+    stack: ['GCP', 'Cloud Run', 'BigQuery', 'Python'],
+    sourceCode: '',
+    livePreview: '',
   },
   {
-    name: 'Project 3',
+    name: 'Data Standardization & ETL Pipeline Cleanup',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'Standardized datasets and resolved inconsistencies across ETL pipelines to improve data quality and reporting accuracy.',
+    stack: ['BigQuery', 'Google Sheets', 'Looker Studio', 'SQL'],
+    sourceCode: '',
+    livePreview: '',
   },
 ]
 
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
-  'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'BigQuery',
+  'Google Cloud Platform (GCP)',
+  'Cloud Composer (Airflow)',
+  'Cloud Run',
+  'Google Cloud Storage (GCS)',
+  'SQL',
+  'Python',
+  'ETL Pipelines',
+  'Data Modeling',
+  'Data Studio',
+  'Google Sheets',
+  'API Integration',
+  'AppScripts',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'rickyvillaverde3@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
