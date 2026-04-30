a94865bbea318bd5dc82002ddc45611b589dfbbf
Ricky Villaverde <rickyvillaverde3@gmail.com>
2026-04-29 21:04:06 -0400

    final final fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,14 @@
 import './Footer.css'
 
 const Footer = () => (
-  <footer className='footer'></footer>
+  <footer className='footer'>
+    <a
+      href='https://github.com/Moojestico/school-portfolios'
+      className='link footer__link'
+    >
+      Created By Ricky Villaverde
+    </a>
+  </footer>
 )
 
 export default Footer
